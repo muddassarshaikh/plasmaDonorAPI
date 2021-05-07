c183b554130dd0846d9090ae1c25b5e8a449953f
Muddassar Shaikh <shaikh.muddassar11@gmail.com>
2021-05-07 10:52:56 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+module.exports = {
+  port: process.env[`${process.env.NODE_ENV}_PORT`],
+  databaseHost: process.env[`${process.env.NODE_ENV}_DB_HOST`],
+  databaseUser: process.env[`${process.env.NODE_ENV}_DB_USER`],
+  databasePassword: process.env[`${process.env.NODE_ENV}_DB_PASSWORD`],
+  databaseName: process.env[`${process.env.NODE_ENV}_DB_NAME`],
+  databaseInitial: process.env[`${process.env.NODE_ENV}_DB_INITIAL`],
+  bodyEncryption: false,
+};
